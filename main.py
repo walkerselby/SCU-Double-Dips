847474daaf36cdc462b8d00273c5e22250648063
Jason Vu <jvu@scu.edu>
2022-06-27 21:51:50 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,60 @@
+import csv 
+import requests
+import os
+
 def get_courses(core):
-    #4400 is the value for Fall Quarter 2022.
+    # 4400 is the value for Fall Quarter 2022.
     url = "https://www.scu.edu/apps/ws/courseavail/search/4400/ugrad"
 
-    payload = "newcore=" + core + "&maxRes=10000"
+    payload = f"newcore={core}&maxRes=10000"
     headers = {
-        'authority': 'www.scu.edu',
-        'accept': '*/*',
-        'x-requested-with': 'XMLHttpRequest',
-        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36',
-        'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-        'sec-gpc': '1',
-        'origin': 'https://www.scu.edu',
-        'sec-fetch-site': 'same-origin',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-dest': 'empty',
-        'accept-language': 'en-US,en;q=0.9',
-            }
+        "authority": "www.scu.edu",
+        "accept": "*/*",
+        "x-requested-with": "XMLHttpRequest",
+        "user-agent": os.getenv("USER_AGENT"),
+        "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+        "sec-gpc": "1",
+        "origin": "https://www.scu.edu",
+        "sec-fetch-site": "same-origin",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-dest": "empty",
+        "accept-language": "en-US,en;q=0.9",
+    }
 
     response = requests.request("POST", url, headers=headers, data=payload)
     data = response.json()
     return data
 
 
-if __name__ == '__main__':
-    import requests
-
+if __name__ == "__main__": 
     courses = {}
 
-    #Core Codes
-    coreDict = {"I_AW": "Advanced Writing",
+    # Core Codes
+    coreDict = {
+             "I_PTHAMS": "American Studies (Pathway)",
+             "I_PTHAE": "Applied Ethics (Pathway)",
+             "I_PTHB": "Beauty (Pathway)",
+             "I_PTHCHD": "Children, Family, & Society (Pathway)",
+             "I_PTHCINST": "Cinema Studies (Pathway)",
+             "I_PTHDEM": "Democracy (Pathway)",
+             "I_PTHDT": "Design Thinking (Pathway)",
+             "I_PTHFHP": "Feeding the World (Pathway)",
+             "I_PTHGGE": "Gender, Globalization & Empire (Pathway)",
+             "I_PTHGSB": "Gender, Sexuality & the Body (Pathway)",
+             "I_PTHGB": "Global Health (Pathway)",
+             "I_PTHHR": "Human Rights (Pathway)", 
+             "I_PTHIS": "Islamic Studies (Pathway)",
+             "I_PTHJA": "Justice and the Arts (Pathway)",
+             "I_PTHLSJ": "Law & Social Justice (Pathway)",
+             "I_PTHLPOSC": "Leading People, Org & Soc Chng (Pathway)",
+             "I_PTHPS": "Paradigm Shifts (Pathway)",
+             "I_PTHPR": "Politics & Religion (Pathway)",
+             "I_PTHRPSI": "Race Place & Soc Inequalities (Pathway)",
+             "I_PTHS": "Sustainability (Pathway)",
+             "I_PTHDA": "The Digital Age (Pathway)",
+             "I_PTHVST": "Values Science Technology (Pathway)",
+             "I_PTHV": "Vocation (Pathway)",
+             "I_AW": "Advanced Writing",
              "E_ARTS": "Arts",
              "E_CE": "Civic Engagement",
              "F_CTW1": "Critical Thinking and Writing 1",
@@ -43,31 +69,41 @@ if __name__ == '__main__':
              "E_RTC2": "Religion Theology & Culture 2",
              "E_RTC3": "Religion Theology & Culture 3",
              "E_STS": "Science Technology & Society",
-             "E_SOSC": "Social Science"
+             "E_SOSC": "Social Science",
+             "E_ARTSPAR": "Partial Credit Arts",
+             "E_CEPAR": "Partial Credit Civic Engagement",
+             "E_STSPAR": "Partial Credit Engineering, Math, CS"
     }
 
     for core in coreDict:
-        print("Fetching " + coreDict[core] + " . . .")
+        print(f"Fetching {coreDict[core]} . . .")
 
-        #Fetch course data and add it to dict
+        # Fetch course data and add it to dict
         data = get_courses(core)
         for info in data["results"]:
-            if info['class_nbr'] in courses:
-                newCore = courses.get(info['class_nbr']).get("core") + ", " + coreDict[core]
-                courses.get(info['class_nbr']).update({"core": newCore})
+            if info["class_nbr"] in courses:
+                newCore = f"{courses.get(info['class_nbr']).get('core')}, {coreDict[core]}"
+                courses.get(info["class_nbr"]).update({"core": newCore})
             else:
                 newCourse = {
-                    "course": info['subject'] + " " + info['catalog_nbr'],
-                    "name": info['class_descr'],
+                    "course": f"{info['subject']} {info['catalog_nbr']} ({info['class_nbr']})",
+                    "name": info["class_descr"],
                     "core": coreDict[core]
                 }
-                courses.update({info['class_nbr']: newCourse})
+                courses.update({info["class_nbr"]: newCourse})
 
-    print("\nDouble Dip Courses:\n")
-    for course in courses:
-        #Checking if there are multiple cores for a course.
-        if "," in courses.get(course).get("core"):
-            row = [courses.get(course).get("course"),courses.get(course).get("name"),courses.get(course).get("core")]
-            #Formatting Output
-            print("{: <10} \t {: <30} \t {: <40}".format(*row))
 
+    rows = ["COURSE TAG", "COURSE NAME", "CORES SATISFIED"] 
+
+    with open("scu_double_dips-fall_2022.csv", "w") as csv_file: 
+        csv_writer = csv.writer(csv_file)
+        csv_writer.writerow(rows) # write header
+
+        for course in courses:
+
+            # Checking if there are multiple cores for a course.
+            if "," in courses.get(course).get("core"):   
+    
+                main_stuff =  [ courses.get(course).get("course"), courses.get(course).get("name"), courses.get(course).get("core") ]
+                
+                csv_writer.writerow([main_stuff[0], main_stuff[1], main_stuff[2]]) # write each item
